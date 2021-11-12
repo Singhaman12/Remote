7b65e07e8cf829cf26d167979246ecf257e1fe48
Singhaman12 <aman.cumech@gmail.com>
2021-11-11 22:20:47 -0500

    Removed sout statment

--- src/Test.java
+++ src/Test.java
@@ -2,8 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println(" Hello Its Aman here ");
         System.out.println("Testing main branch");
-        System.out.println("changes by joban");
-        System.out.println("testing git push");
-        System.out.println("Testing Main Branch");
+
+
     }
 }
